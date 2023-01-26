1969d1e29672719f6f1c558523e2f165bbcd669f
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-01-26 15:56:13 -0300

    Mostrar as informações das guitarras

--- app/components/guitarra.jsx
+++ app/components/guitarra.jsx
@@ -1,5 +1,27 @@
+import { Link } from "@remix-run/react";
+
 function Guitarra({ guitarra }) {
-  return <div>Guitarra</div>;
+  const { descripcion, imagen, precio, url, nombre } = guitarra;
+
+  console.log(imagen.data.attributes.formats.medium.url);
+
+  return (
+    <div className="guitarra">
+      <img
+        src={imagen.data.attributes.formats.medium.url}
+        alt={`Imagem da guitarra ${nombre}`}
+      />
+      <div className="contenido">
+        <h3>{nombre}</h3>
+        <p className="descripcion">{descripcion}</p>
+        <p className="precio">${precio}</p>
+
+        <Link className="enlace" to={`/guitarras/${url}`}>
+          Ver Producto
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Guitarra;
