fa23ae25904385484dc7e132420fca6d090eeb75
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-01-26 14:37:48 -0300

    Consumindo a API construída com Strapi

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,3 +1,13 @@
+export async function loader() {
+  const respuesta = await fetch(
+    `${process.env.API_URL}/guitarras?populate=imagen`
+  );
+  const resultado = await respuesta.json();
+  console.log(resultado);
+  console.log(process.env.API_URL);
+  return {};
+}
+
 function Tienda() {
   return <div>Tienda</div>;
 }
