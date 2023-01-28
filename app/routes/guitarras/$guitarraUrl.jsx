f1b531bf6356990358b2c654a8acc3e1e7ab5e2e
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-01-27 22:45:33 -0300

    Mostrar as informações de uma guitarra em um página

--- app/routes/guitarras/$guitarraUrl.jsx
+++ app/routes/guitarras/$guitarraUrl.jsx
@@ -1,5 +1,15 @@
 import { useLoaderData } from "@remix-run/react";
 import { getGuitarra } from "~/models/guitarras.server";
+import styles from "~/styles/guitarras.css";
+
+export function links() {
+  return [
+    {
+      rel: "stylesheet",
+      href: styles,
+    },
+  ];
+}
 
 export async function loader({ params }) {
   const { guitarraUrl } = params;
@@ -9,9 +19,22 @@ export async function loader({ params }) {
 
 function Guitarra() {
   const guitarra = useLoaderData();
-  console.log(guitarra.data[0].attributes.nombre);
+  const { nombre, descripcion, imagen, precio } = guitarra.data[0].attributes;
 
-  return <div>GuitarraUrl</div>;
+  return (
+    <main className="contenedor guitarra">
+      <img
+        className="imagen"
+        src={imagen.data.attributes.url}
+        alt={`Imagen da la guitarra ${nombre}`}
+      />
+      <div className="contenido">
+        <h3>{nombre}</h3>
+        <p className="texto">{descripcion}</p>
+        <p className="precio">${precio}</p>
+      </div>
+    </main>
+  );
 }
 
 export default Guitarra;
