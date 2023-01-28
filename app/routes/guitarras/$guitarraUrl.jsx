eed88650a4d055ac8fb56c373a3c4948cede647c
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-01-27 22:51:43 -0300

    Criar a função meta em $guitarraUrl.jsx para gerar as informações de forma dinâmica

--- app/routes/guitarras/$guitarraUrl.jsx
+++ app/routes/guitarras/$guitarraUrl.jsx
@@ -2,6 +2,13 @@ import { useLoaderData } from "@remix-run/react";
 import { getGuitarra } from "~/models/guitarras.server";
 import styles from "~/styles/guitarras.css";
 
+export function meta({ data }) {
+  return {
+    title: `GuitarLA - ${data.data[0].attributes.nombre}`,
+    description: `Guitarras, venta de guitarras, guitarra ${data.data[0].attributes.nombre}`,
+  };
+}
+
 export function links() {
   return [
     {
