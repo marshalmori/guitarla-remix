ac1d0f122ac897f01dbbf2aa5f513299d6ce3ae9
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-01-28 22:02:27 -0300

    Finalizar os dados da página do Post

--- app/routes/posts/$postUrl.jsx
+++ app/routes/posts/$postUrl.jsx
@@ -1,4 +1,29 @@
+import { useLoaderData } from "@remix-run/react";
 import { getPost } from "~/models/posts.server";
+import { formatearFecha } from "~/utils/helpers";
+import styles from "~/styles/blog.css";
+
+export function links() {
+  return [
+    {
+      rel: "stylesheet",
+      href: styles,
+    },
+  ];
+}
+
+export function meta({ data }) {
+  if (!data) {
+    return {
+      title: "GuitarLA - Entrada não encotrada",
+      description: "Guitarras, venta de guitarras, entrada não encontrada",
+    };
+  }
+  return {
+    title: `GuitarLA - ${data.data[0].attributes.titulo}`,
+    description: `Guitarras, venta de guitarras, ${data.data[0].attributes.titulo}`,
+  };
+}
 
 export async function loader({ params }) {
   const { postUrl } = params;
@@ -15,5 +40,22 @@ export async function loader({ params }) {
 }
 
 export default function Post() {
-  return <div>$postUrl</div>;
+  const post = useLoaderData();
+
+  const { titulo, contenido, imagen, publishedAt } = post.data[0]?.attributes;
+
+  return (
+    <article className="contenedor post mt-3">
+      <img
+        className="imagen"
+        src={imagen?.data?.attributes?.url}
+        alt={`Imagen Post ${titulo}`}
+      />
+      <div className="contenido">
+        <h3>{titulo}</h3>
+        <p className="fecha">{formatearFecha(publishedAt)}</p>
+        <p className="texto">{contenido}</p>
+      </div>
+    </article>
+  );
 }
