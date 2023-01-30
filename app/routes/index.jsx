90c3acbc2e9a99b175f146d286454becd4987329
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-01-30 16:56:34 -0300

    Chamar no index o conteúdo do curso e aplicar CSS junto com jsx

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,10 +1,13 @@
 import { getGuitarras } from "~/models/guitarras.server";
 import { getPosts } from "~/models/posts.server";
+import { getCurso } from "~/models/curso.server";
 import { useLoaderData } from "@remix-run/react";
 import ListadoGuitarras from "~/components/listado-guitarras";
 import ListadoPosts from "../components/listado-posts";
 import stylesGuitarras from "~/styles/guitarras.css";
+import Curso from "../components/curso";
 import stylesPosts from "~/styles/blog.css";
+import stylesCurso from "~/styles/curso.css";
 import { Fragment } from "react";
 
 export function meta() {}
@@ -19,26 +22,38 @@ export function links() {
       rel: "stylesheet",
       href: stylesPosts,
     },
+    {
+      rel: "stylesheet",
+      href: stylesCurso,
+    },
   ];
 }
 
 export async function loader() {
-  const [guitarras, posts] = await Promise.all([getGuitarras(), getPosts()]);
+  const [guitarras, posts, curso] = await Promise.all([
+    getGuitarras(),
+    getPosts(),
+    getCurso(),
+  ]);
 
   return {
     guitarras: guitarras.data,
     posts: posts.data,
+    curso: curso.data,
   };
 }
 
 function Index() {
-  const { guitarras, posts } = useLoaderData();
+  const { guitarras, posts, curso } = useLoaderData();
 
   return (
     <Fragment>
       <main className="contenedor">
         <ListadoGuitarras guitarras={guitarras} />
       </main>
+
+      <Curso curso={curso.attributes} />
+
       <section className="contenedor">
         <ListadoPosts posts={posts} />
       </section>
