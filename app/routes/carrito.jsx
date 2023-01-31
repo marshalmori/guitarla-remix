246a19b17a3cc580fafaebcf4088935f9ecb6281
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-01-30 22:09:13 -0300

    Criar a página do carrinho de compras e aplicar CSS

--- app/routes/carrito.jsx
+++ app/routes/carrito.jsx
@@ -1,5 +1,38 @@
+import styles from "~/styles/carrito.css";
+
+export function links() {
+  return [
+    {
+      rel: "stylesheet",
+      href: styles,
+    },
+  ];
+}
+
+export function meta() {
+  return {
+    title: "GuitarLA - Carrito de Compras",
+    description: "Venta de guitarras, música, blog, carrito de compras, tienda",
+  };
+}
+
 const Carrito = () => {
-  return <div>Carrito</div>;
+  return (
+    <main className="contenedor">
+      <h1 className="heading">Carrito de Compras</h1>
+
+      <div className="contenido">
+        <div className="carrito">
+          <h2>Articulos</h2>
+        </div>
+
+        <aside className="resumen">
+          <h3>Resumen del Pedido</h3>
+          <p>Total a pagar: $</p>
+        </aside>
+      </div>
+    </main>
+  );
 };
 
 export default Carrito;
